2d8c00b86330948498d69584beef583fbf9df739
Ciprian Olariu <olariu_ciprian_05@yahoo.com>
2016-04-16 13:55:47 +0300

    Created project. Added a basic main menu

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
